6897ddec4abaaf02e636b047c9f34317b6f9a168
Black Boy <minhhieuec@gmail.com>
2019-01-06 10:24:45 +0700

    add read button with internal resistor pullups

---
+++ GPIO INPUT - READ BUTTON WITH DEBOUNCE - INTERNAL RESISTOR PULLUP/user_main.c
@@ -0,0 +1,127 @@
+/*
+ * ESPRESSIF MIT License
+ *
+ * Copyright (c) 2015 <ESPRESSIF SYSTEMS (SHANGHAI) PTE LTD>
+ *
+ * Permission is hereby granted for use on ESPRESSIF SYSTEMS ESP8266 only, in which case,
+ * it is free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * @date:       06/01/2019
+ * @author:     MinhHieuEC
+ * @blog:       https://ratdongian.blogspot.com
+ * @github:     https://github.com/minhhieuec
+ * @youtube:    https://www.youtube.com/channel/UCP1J5jV7UnQbk66-N540rPw
+ */
+#include "ets_sys.h"
+#include "osapi.h"
+#include "spi_test.h"
+#include "user_interface.h"
+
+#if ((SPI_FLASH_SIZE_MAP == 0) || (SPI_FLASH_SIZE_MAP == 1))
+#error "The flash map is not supported"
+#elif (SPI_FLASH_SIZE_MAP == 2)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0xfb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0xfc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0xfd000
+#elif (SPI_FLASH_SIZE_MAP == 3)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x1fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x1fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x1fd000
+#elif (SPI_FLASH_SIZE_MAP == 4)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x3fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x3fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x3fd000
+#elif (SPI_FLASH_SIZE_MAP == 5)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x101000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x1fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x1fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x1fd000
+#elif (SPI_FLASH_SIZE_MAP == 6)
+#define SYSTEM_PARTITION_OTA_SIZE							0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR							0x101000
+#define SYSTEM_PARTITION_RF_CAL_ADDR						0x3fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR						0x3fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR				0x3fd000
+#else
+#error "The flash map is not supported"
+#endif
+
+static const partition_item_t at_partition_table[] = {
+    { SYSTEM_PARTITION_BOOTLOADER, 						0x0, 												0x1000},
+    { SYSTEM_PARTITION_OTA_1,   						0x1000, 											SYSTEM_PARTITION_OTA_SIZE},
+    { SYSTEM_PARTITION_OTA_2,   						SYSTEM_PARTITION_OTA_2_ADDR, 						SYSTEM_PARTITION_OTA_SIZE},
+    { SYSTEM_PARTITION_RF_CAL,  						SYSTEM_PARTITION_RF_CAL_ADDR, 						0x1000},
+    { SYSTEM_PARTITION_PHY_DATA, 						SYSTEM_PARTITION_PHY_DATA_ADDR, 					0x1000},
+    { SYSTEM_PARTITION_SYSTEM_PARAMETER, 				SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR, 			0x3000},
+};
+
+void ICACHE_FLASH_ATTR user_pre_init(void)
+{
+    if(!system_partition_table_regist(at_partition_table, sizeof(at_partition_table)/sizeof(at_partition_table[0]),SPI_FLASH_SIZE_MAP)) {
+		os_printf("system_partition_table_regist fail\r\n");
+		while(1);
+	}
+}
+
+os_timer_t check_button_timer;
+
+bool led_stt = false;
+uint32 btn_data = 0;
+uint8 count = 0;
+
+//  check push button function
+void check_button(){
+
+    btn_data = GPIO_INPUT_GET(12);  //  read button value at gpio 12 pin
+
+    if (btn_data == 0){
+        os_delay_us(10000);     //  wait for debounce
+
+        btn_data = GPIO_INPUT_GET(12);  //  read button value at gpio 12 pin
+
+        if (btn_data == 0) {
+            led_stt = !led_stt;
+            GPIO_OUTPUT_SET(13, led_stt);   //  controll led at pin gpio 13
+            count += 1;                     //  if push button => count = count + 1
+            os_printf("button push %d\n", count);   //  debug count
+        }
+    }
+  while (GPIO_INPUT_GET(12) == 0);  //  wait to unpush button
+}
+
+
+void ICACHE_FLASH_ATTR
+user_init(void)
+{
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);    //  select function gpio for gpio 13 pin
+    GPIO_OUTPUT_SET(13, 0);                                 //  set gpio 13 is output and pulldown GND
+
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);    //  select function gpio for gpio 12
+    PIN_PULLUP_EN(PERIPHS_IO_MUX_MTDI_U);                   //  use internal pullup for gpio 12
+
+    GPIO_DIS_OUTPUT(12);                                    //  use gpio 12 as input
+
+    os_timer_setfn(&check_button_timer, (os_timer_func_t *) check_button, NULL);    //  set callback timer to check button
+    os_timer_arm(&check_button_timer, 100, true);                                   //  100 ms callback to function check_button_timer
+}
